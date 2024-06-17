c6bdbcc20bdf5ab1eceec01809ca745c6d61a9c3
ntantis <ntantis@intralot.com>
2024-06-17 13:39:41 +0300

    vis

--- inference.py
+++ inference.py
@@ -9,30 +9,82 @@ from torchvision.transforms import Resize, RandomHorizontalFlip, RandomVerticalF
 from torchmetrics import MetricCollection, PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
 from torch.optim.lr_scheduler import StepLR
 from torchinfo import summary
+from torchmetrics.image import SpectralAngleMapper, ErrorRelativeGlobalDimensionlessSynthesis
 
 from data_loader.DataLoader import DIV2K, GaoFen2, Sev2Mod, WV3, GaoFen2panformer
 from models.panformer import CrossSwinTransformer
 from utils import *
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+
+
+def measure_gpu_throughput(model, input1_, input2_):
+    input1 = input1_.to('cuda')
+    input2 = input2_.to('cuda')
+    model = model.to('cuda')
+
+    ave_forward_throughput = []
+
+    ave_start = time.time()
+    for t in range(300):
+        start = time.time()
+        x = model(input1, input2)
+        end = time.time()
+        fwd_throughput = 1/(end-start)
+        # print('forward_throughput is {:.4f}'.format(fwd_throughput))
+        ave_forward_throughput.append(fwd_throughput)
+
+    ave_fwd_throughput = np.mean(ave_forward_throughput[1:])
+
+    print('Mean throughput over 300 runs: {:.4f}'.format(ave_fwd_throughput))
+
+
+def measure_gpu_latency(model, input1_, input2_):
+    input1 = input1_.to('cuda')
+    input2 = input2_.to('cuda')
+    model = model.to('cuda')
+
+    repetitions = 300
+
+    # GPU-WARM-UP
+    for _ in range(20):  # Increase warm-up iterations to ensure GPU is fully warmed up
+        _ = model(input1, input2)
+        torch.cuda.synchronize()  # Ensure the warm-up operation completes
+
+    # Measure performance
+    timings = []
+    with torch.no_grad():
+        for _ in range(repetitions):
+            start = time.time()
+            _ = model(input1, input2)
+            torch.cuda.synchronize()  # Ensure the operation completes
+            end = time.time()
+            latency = end - start
+            timings.append(latency)
+
+    mean_latency = np.mean(timings)
+    print(
+        f"Mean time over {repetitions} runs: {mean_latency} seconds")
+    return mean_latency
 
 
 def main():
-    choose_dataset = 'GaoFen2' #or 'WV3'
+    choose_dataset = 'WV3'  # or 'WV3'
 
     if choose_dataset == 'GaoFen2':
         dataset = eval('GaoFen2')
-        tr_dir = '../pansharpenning_dataset/GF2/train/train_gf2.h5'
-        eval_dir = '../pansharpenning_dataset/GF2/val/valid_gf2.h5'
-        test_dir = '../pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
+        tr_dir = 'data/pansharpenning_dataset/GF2/train/train_gf2.h5'
+        eval_dir = 'data/pansharpenning_dataset/GF2/val/valid_gf2.h5'
+        test_dir = 'data/pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
         checkpoint_dir = 'checkpoints/panformer_GF2/panformer_GF2_2023_07_19-00_31_49.pth.tar'
         ms_channel = 4
         ergas_l = 4
     elif choose_dataset == 'WV3':
         dataset = eval('WV3')
-        tr_dir = '../pansharpenning_dataset/WV3/train/train_wv3.h5'
-        eval_dir = '../pansharpenning_dataset/WV3/val/valid_wv3.h5'
-        test_dir = '../pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
+        tr_dir = 'data/pansharpenning_dataset/WV3/train/train_wv3.h5'
+        eval_dir = 'data/pansharpenning_dataset/WV3/val/valid_wv3.h5'
+        test_dir = 'data/pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
         checkpoint_dir = 'checkpoints/panformer_WV3/panformer_WV3_2023_07_23-16_44_45.pth.tar'
         ms_channel = 8
         ergas_l = 4
@@ -40,7 +92,6 @@ def main():
         print(choose_dataset, ' does not exist')
 
     # Prepare device
-    # TODO add more code for server
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
@@ -71,17 +122,23 @@ def main():
 
     metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
     })
 
     val_metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
     })
 
     test_metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
     })
 
     tr_report_loss = 0
@@ -91,11 +148,9 @@ def main():
     val_metrics = []
     test_metrics = []
 
-
     ergas_score = 0
     sam_score = 0
     q2n_score = 0
-    
 
     best_eval_psnr = 0
     best_test_psnr = 0
@@ -132,6 +187,15 @@ def main():
     def scaleMinMax(x):
         return ((x - np.nanmin(x)) / (np.nanmax(x) - np.nanmin(x)))
 
+    input_tensor1 = torch.randn(
+        1, 1, 256, 256).to(device)  # Example input tensor 1
+    input_tensor2 = torch.randn(
+        1, ms_channel, 64, 64).to(device)  # Example input tensor 2
+    model.eval()
+
+    measure_gpu_throughput(model, input_tensor1, input_tensor2)
+    measure_gpu_latency(model, input_tensor1, input_tensor2)
+
     idx = 0
     # evaluation mode
     model.eval()
@@ -146,10 +210,6 @@ def main():
             test_metric = test_metric_collection.forward(mssr, mshr)
             test_report_loss += test_loss
 
-            ergas_score += ergas_batch(mshr, mssr, ergas_l)
-            sam_score += sam_batch(mshr, mssr)
-            q2n_score += q2n_batch(mshr, mssr)
-
             figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
             axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
                             0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
@@ -157,7 +217,7 @@ def main():
             axis[0].axis("off")
 
             axis[1].imshow(pan.permute(0, 3, 2, 1).detach().cpu()[
-                            0, ...], cmap='gray')
+                0, ...], cmap='gray')
             axis[1].set_title('(b) PAN')
             axis[1].axis("off")
 
@@ -180,7 +240,7 @@ def main():
             gt = mshr.permute(0, 3, 2, 1).detach().cpu().numpy()
 
             np.savez(f'results/img_array_{choose_dataset}_{i}.npz', mslr=mslr,
-                        pan=pan, mssr=mssr, gt=gt)
+                     pan=pan, mssr=mssr, gt=gt)
 
         # compute metrics
         test_metric = test_metric_collection.compute()
@@ -188,11 +248,11 @@ def main():
 
         # Print final scores
         print(f"Final scores:\n"
-                f"ERGAS: {ergas_score / (i+1)}\n"
-                f"SAM: {sam_score / (i+1)}\n"
-                f"Q2n: {q2n_score / (i+1)}\n"
-                f"PSNR: {test_metric['psnr'].item()}\n"
-                f"SSIM: {test_metric['ssim'].item()}")
+              f"ERGAS: {test_metric['ergas'].item()}\n"
+              f"SAM: {test_metric['sam'].item()}\n"
+              f"PSNR: {test_metric['psnr'].item()}\n"
+              f"SSIM: {test_metric['ssim'].item()}")
+
 
 if __name__ == '__main__':
     main()
